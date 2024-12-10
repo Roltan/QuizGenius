66f58b1c6ae1dd17f96619962e83c548036deebb
Roltan <KhalikoR@yandex.ru>
2024-12-10 16:32:33 +0500

    прокачал статистику

--- resources/views/profile-solveds.blade.php
+++ resources/views/profile-solveds.blade.php
@@ -6,40 +6,20 @@
 @endsection
 
 @section('mainContent')
-    @include('/block/header_lk', ['active'=>3])
+    @include('/block/header_lk', ['active'=>4])
 
     <figure class="background"></figure>
 
     <main class="container">
-        @include('/block/navLK', ['active'=>3])
+        @include('/block/navLK', ['active'=>4])
 
         <div class="main">
-            <div class="statistic--header">
-                <div class="input input__img">
-                    <label for="search"><img src="/img/lens.png" alt="" /></label>
-                    <input type="text" placeholder="поиск" name="search" id="search" class="input--field" />
-                </div>
-                <img src="/img/filter.png" alt="" class="openModalBtn" data-modal="modal2" />
-
-                <div class="modalka" id="modal2">
-                    <form class="filter form">
-                        @include('/elements/input/selector', [
-                            'name'=>'test',
-                            'label'=>'Тест',
-                            'options'=>[
-                                'тест 1',
-                                'тест 2',
-                                'тест 3',
-                                'тест 4',
-                            ]
-                        ])
-
-                        @include('/elements/input/data_select')
-
-                        <button type="submit" class="button button__light button__bold">Применить</button>
-                    </form>
-                </div>
-            </div>
+            @include('/block/list_header', ['tests'=>[
+                'тест 1',
+                'тест 2',
+                'тест 3',
+                'тест 4',
+            ]])
 
             <div class="window">
                 <div class="list">
--- resources/views/profile-statistic.blade.php
+++ resources/views/profile-statistic.blade.php
@@ -1,201 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="/css/statistic.css" />
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-		<title>Document</title>
-		<script defer src="/js/modal.js"></script>
-	</head>
-	<body>
-		<header class="header header--lk">
-			<div class="container">
-				<div class="header--logo">
-					<img src="img/лого.png" alt="" />
-					<img src="img/burger.png" alt="" class="openModalBtn" data-modal="modal1" />
-				</div>
+@extends('/block/pattern')
 
-				<div class="header--buttons">
-					<span class="header--text">Личный кабинет</span>
-				</div>
-			</div>
-		</header>
-		<div class="modalka" id="modal1">
-			<div class="navLK navLK__bar">
-				<div>
-					<figure></figure>
-					<a href="/profile" class="active">
-						<img src="/img/humen.png" alt="" />
-						<span>Данные аккаунта</span>
-					</a>
-					<a href="/profile/create">
-						<img src="/img/lk/create.png" alt="" />
-						<span>Создать тест</span>
-					</a>
-					<a href="/profile/statistic">
-						<img src="/img/lk/statistic.png" alt="" />
-						<span>Статистика</span>
-					</a>
-					<a href="/profile/solved">
-						<img src="/img/lk/solved.png" alt="" />
-						<span>Мои решения</span>
-					</a>
-				</div>
-				<a href="/logout">
-					<img src="/img/lk/exit.png" alt="" />
-					<span>Выйти из аккаунта</span>
-				</a>
-			</div>
-		</div>
+@section('links')
+    <link rel="stylesheet" href="/css/statistic.css" />
+    <script defer src="/js/modal.js"></script>
+@endsection
 
-		<figure class="background"></figure>
+@section('mainContent')
+    @include('/block/header_lk', ['active'=>3])
 
-		<main class="container">
-			<nav class="navLK">
-				<div>
-					<figure></figure>
-					<a href="/profile">
-						<img src="/img/humen.png" alt="" />
-						<span>Данные аккаунта</span>
-					</a>
-					<a href="/profile/create">
-						<img src="/img/lk/create.png" alt="" />
-						<span>Создать тест</span>
-					</a>
-					<a href="/profile/statistic" class="active">
-						<img src="/img/lk/statistic.png" alt="" />
-						<span>Статистика</span>
-					</a>
-					<a href="/profile/solved">
-						<img src="/img/lk/solved.png" alt="" />
-						<span>Мои решения</span>
-					</a>
-				</div>
-				<a href="/logout">
-					<img src="/img/lk/exit.png" alt="" />
-					<span>Выйти из аккаунта</span>
-				</a>
-			</nav>
+    <figure class="background"></figure>
 
-			<div class="main">
-				<div class="statistic--header">
-					<div class="input input__img">
-						<label for="search"><img src="/img/lens.png" alt="" /></label>
-						<input type="text" placeholder="поиск" name="search" id="search" class="input--field" />
-					</div>
-					<img src="/img/filter.png" alt="" class="openModalBtn" data-modal="modal2" />
+    <main class="container">
+        @include('/block/navLK', ['active'=>3])
 
-					<div class="modalka" id="modal2">
-						<form class="filter form">
-							<div class="input">
-								<label for="test">Тест</label>
-								<select name="test" id="test" class="input--field">
-									<option value="" disabled selected hidden>Выбрать вариант</option>
-									<option value="1">тест 1</option>
-									<option value="2">тест 2</option>
-									<option value="3">тест 3</option>
-									<option value="4">тест 4</option>
-								</select>
-							</div>
+        <div class="main">
+            @include('/block/list_header', ['tests'=>[
+                'тест 1',
+                'тест 2',
+                'тест 3',
+                'тест 4',
+            ]])
 
-							<div class="input input__data">
-								<label for="">Дата</label>
-								<div>
-									<select name="day" id="day" class="input--field">
-										<option value="" disabled selected hidden>День</option>
-										<option value="1">тест 1</option>
-										<option value="2">тест 2</option>
-										<option value="3">тест 3</option>
-										<option value="4">тест 4</option>
-									</select>
-									<select name="month" id="month" class="input--field">
-										<option value="" disabled selected hidden>Месяц</option>
-										<option value="1">тест 1</option>
-										<option value="2">тест 2</option>
-										<option value="3">тест 3</option>
-										<option value="4">тест 4</option>
-									</select>
-									<select name="year" id="year" class="input--field">
-										<option value="" disabled selected hidden>Год</option>
-										<option value="1">тест 1</option>
-										<option value="2">тест 2</option>
-										<option value="3">тест 3</option>
-										<option value="4">тест 4</option>
-									</select>
-								</div>
-							</div>
-
-							<button type="submit" class="button button__light button__bold">Применить</button>
-						</form>
-					</div>
-				</div>
-
-				<div class="window">
-					<div class="list">
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-						<a href="#" class="list--card">
-							<span>Имя</span>
-							<span>XX/XX</span>
-							<span>Название теста</span>
-							<span>Дата</span>
-						</a>
-					</div>
-				</div>
-			</div>
-		</main>
-	</body>
-</html>
+            <div class="window">
+                <div class="list">
+                    @for($i=0; $i<10; $i++)
+                        @include('/elements/solved', [
+                            'href'=>'#',
+                            'span'=>[
+                                'Имя',
+                                'XX/XX',
+                                'Название теста',
+                                'Дата'
+                            ]
+                        ])
+                    @endfor
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
